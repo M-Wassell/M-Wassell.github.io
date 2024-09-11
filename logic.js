0b801654e30533d18d0edda361002493dbcc9f9b
M-Wassell <55836302+M-Wassell@users.noreply.github.com>
2024-09-11 16:53:28 +0100

    Contacts button that copies email address - logic is implemented but needs polish, such as CSS to make it look appealing.

---
+++ logic.js
@@ -0,0 +1,10 @@
+function openContact() {
+  const popupInfo = document.getElementById("popupInfo");
+  popupInfo.style.visibility 
+    popupInfo.style.visibility === "visible" ? "hidden" : "visible";
+}
+// function openPop() {
+//   const popDialog = document.getElementById("popupDialog");
+//   popDialog.style.visibility =
+//     popDialog.style.visibility === "visible" ? "hidden" : "visible";
+// }
